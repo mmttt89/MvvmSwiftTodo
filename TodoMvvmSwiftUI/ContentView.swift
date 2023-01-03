3e5b5572ea1c6a04ee7695c17e86868f6383af82
mehdi <m.taghdisi@digikala.com>
2023-01-04 00:32:28 +0330

    Initial Commit

---
+++ TodoMvvmSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TodoMvvmSwiftUI
+//
+//  Created by Mehdi Taghdisi on 10/14/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoMvvmSwiftUI/TodoMvvmSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoMvvmSwiftUIApp.swift
+//  TodoMvvmSwiftUI
+//
+//  Created by Mehdi Taghdisi on 10/14/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct TodoMvvmSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
